eff50875e22074b6ffd52d0611714ed2ebecd636
tjbass2021 <tjbass2021@gmail.com>
2022-01-28 02:26:59 -0300

    algumas correções

--- js/contador.js
+++ js/contador.js
@@ -2,9 +2,19 @@ let contador = 40;
 
 const CURRENT_NUMBER = document.getElementById('tela');
 
+
+// A repetição de 'if' com a condição de ser igual ou menor que zero para a exibição
+// da mensagem de derrota é para evitar que volte-se a exibir os valores numéricos
+// na medida em que se aperte os botões de incremento e o valor continue igual ou abaixo de zero.
+
 function incrementPlus(){
     contador = contador + 5;
     CURRENT_NUMBER.innerHTML = contador;
+
+    if(contador<= 0){
+        // contador = 'You Lose!'
+        CURRENT_NUMBER.innerHTML = 'You Lose!';
+    }
 }
 
 function decrementPlus() {
@@ -12,14 +22,19 @@ function decrementPlus() {
     CURRENT_NUMBER.innerHTML = contador;
 
     if(contador<= 0){
-        contador = 'You Lose!'
-        CURRENT_NUMBER.innerHTML = contador;
+        // contador = 'You Lose!'
+        CURRENT_NUMBER.innerHTML = 'You Lose!';
     }
 }
 
 function increment(){
     contador++;
     CURRENT_NUMBER.innerHTML = contador;
+
+    if(contador<= 0){
+        // contador = 'You Lose!'
+        CURRENT_NUMBER.innerHTML = 'You Lose!';
+    }
 }
 
 function decrement(){
@@ -27,8 +42,8 @@ function decrement(){
     CURRENT_NUMBER.innerHTML = contador;
 
     if(contador <= 0){
-        contador = 'You Lose!'
-        CURRENT_NUMBER.innerHTML = contador;
+        // contador = 'You Lose!'
+        CURRENT_NUMBER.innerHTML = "You Lose!";
     }
 }
 
